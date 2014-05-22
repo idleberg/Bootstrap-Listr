10ee175516bc27abcc65325ac535327f4b451486
idleberg <jan@idleberg.com>
2014-05-22 21:48:50 +0200

    added default iPhone icon size

--- index.php
+++ index.php
@@ -485,7 +485,7 @@ $header = "  <meta charset=\"utf-8\">" . PHP_EOL;
 $header = $header."  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=yes\">" . PHP_EOL;
 $header = $header."  <title>Index of $this_domain$this_folder</title>" . PHP_EOL;
 if (FAV_ICON) $header = $header."  <link rel=\"shortcut icon\" href=\"".FAV_ICON."\" />" . PHP_EOL;
-if (IPHONE_ICON) $header = $header."  <link rel=\"apple-touch-icon\" href=\"".IPHONE_ICON."\" />" . PHP_EOL;
+if (IPHONE_ICON) $header = $header."  <link rel=\"apple-touch-icon\" sizes=\"57x57\" href=\"".IPHONE_ICON."\" />" . PHP_EOL;
 if (IPHONE_ICON_RETINA) $header = $header."  <link rel=\"apple-touch-icon\" sizes=\"72x72\" href=\"".IPHONE_ICON_RETINA."\" />" . PHP_EOL;
 if (IPAD_ICON) $header = $header."  <link rel=\"apple-touch-icon\" sizes=\"114x114\" href=\"".IPAD_ICON."\" />" . PHP_EOL;
 if (IPAD_ICON_RETINA) $header = $header."  <link rel=\"apple-touch-icon\" sizes=\"144x144\" href=\"".IPAD_ICON_RETINA."\" />" . PHP_EOL;
