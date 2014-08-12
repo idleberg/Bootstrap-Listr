1a90b6d25c5c0b78073b643c485f03b7450ca64c
idleberg <jan@idleberg.com>
2014-08-13 00:53:55 +0200

    removed website and feed for fa-files icons

--- index.php
+++ index.php
@@ -212,14 +212,12 @@ if (DOC_ICONS == 'fontawesome') {
         'archive'    => array('7z','ace','adf','air','apk','arj','bz2','bzip','cab','d64','dmg','git','hdf','ipf','iso','fdi','gz','jar','lha','lzh','lz','lzma','pak','phar','pkg','pimp','rar','safariextz','sfx','sit','sitx','sqx','sublime-package','swm','tar','tgz','wim','wsz','xar','zip'),
         'audio'      => array('aac','ac3','aif','aiff','au','caf','flac','it','m4a','m4p','med','mid','mo3','mod','mp1','mp2','mp3','mpc','ned','ra','ram','oga','ogg','oma','s3m','sid','umx','wav','webma','wv','xm'),
         'excel'      => array('xls','xlsx','numbers'),
-        'feed'       => array('atom','rss'),
         'image'      => array('ai','bmp','cdr','emf','eps','gif','icns','ico','jp2','jpe','jpeg','jpg','jpx','pcx','pict','png','psd','psp','svg','tga','tif','tiff','webp','wmf'),
         'pdf'        => array('pdf'),
         'powerpoint' => array('pot','ppt','pptx','key'),
         'script'     => array('ahk','as','asp','aspx','bat','c','cfm','clj','cmd','cpp','css','el','erb','g','hml','java','js','json','jsp','less','nsh','nsi','php','php3','pl','py','rb','rhtml','sass','scala','scm','scpt','scptd','scss','sh','shtml','wsh','xml','yml'),
         'text'       => array('ans','asc','ascii','csv','diz','latex','log','markdown','md','nfo','rst','rtf','tex','text','txt'),
         'video'      => array('3g2','3gp','3gp2','3gpp','asf','avi','bik','bup','divx','flv','ifo','m4v','mkv','mkv','mov','mp4','mpeg','mpg','rm','rv','ogv','qt','smk','swf','vob','webm','wmv','xvid'),
-        'website'    => array('htm','html','mhtml','mht','xht','xhtml'),
         'word'       => array('doc','docm','docs','docx','dot','pages'),
     );
     $home = "<i class=\"fa fa-home fa-lg fa-fw\"></i> ";
@@ -339,8 +337,6 @@ if ($handle = opendir('.'))
                     $item['class'] = 'fa fa-file-audio-o ' . FONTAWESOME_STYLE;
                 }elseif(in_array($item['lext'], $filetype['excel'])){
                     $item['class'] = 'fa fa-file-excel-o ' . FONTAWESOME_STYLE;
-                }elseif(in_array($item['lext'], $filetype['feed'])){
-                    $item['class'] = 'fa fa-rss ' . FONTAWESOME_STYLE;
                 }elseif(in_array($item['lext'], $filetype['image'])){
                     $item['class'] = 'fa fa-file-image-o ' . FONTAWESOME_STYLE;
                 }elseif(in_array($item['lext'], $filetype['pdf'])){
@@ -353,8 +349,6 @@ if ($handle = opendir('.'))
                     $item['class'] = 'fa fa-file-text-o ' . FONTAWESOME_STYLE;
                 }elseif(in_array($item['lext'], $filetype['video'])){
                     $item['class'] = 'fa fa-file-video-o ' . FONTAWESOME_STYLE;
-                }elseif(in_array($item['lext'], $filetype['website'])){
-                    $item['class'] = 'fa fa-globe ' . FONTAWESOME_STYLE;
                 }elseif(in_array($item['lext'], $filetype['word'])){
                     $item['class'] = 'fa fa-file-word-o ' . FONTAWESOME_STYLE;
                 }else{
