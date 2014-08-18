ab1d1bb6eae04b39d59c13b00f44113dca3b097d
idleberg <jan@idleberg.com>
2014-08-18 22:16:48 +0200

    experimental localization

---
+++ src/listr-l10n.php
@@ -0,0 +1,30 @@
+<?php
+
+session_start();
+
+if ($options['general']['locale']) {
+    $locale = $options['general']['locale'];
+} else if (isset($_GET["locale"])) {
+    $locale = $_GET["locale"];
+} else if (isset($_SESSION["locale"])) {
+    $locale = $_SESSION["locale"];
+} else {
+    $locale = "en_US";
+}
+
+putenv("LANG=" . $locale);
+setlocale(LC_ALL, $locale);
+
+$domain = "messages";
+bindtextdomain($domain, "locale");
+bind_textdomain_codeset($domain, 'UTF-8');
+
+textdomain($domain);
+
+// echo _("Let's test this thing");
+
+// $var1 = "#variable1";
+// $var2 = "#variable2";
+// $index = sprintf(_('Index of %1$s%2$s'), $_SERVER['HTTP_HOST'], $_SERVER['REQUEST_URI']);
+echo $index;
+?>
