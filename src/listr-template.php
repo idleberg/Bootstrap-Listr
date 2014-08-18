32992081e789b2f1e567370d7c9c2ff112064eae
idleberg <jan@idleberg.com>
2014-08-18 23:53:59 +0200

    text direction improvements

--- src/listr-template.php
+++ src/listr-template.php
@@ -16,7 +16,7 @@
         <tfoot>
           <tr>
             <td colspan="<?=$table_count+1?>">
-              <small class="pull-<?=$$left?> text-muted" dir="ltr"><?=$contained?></small>
+              <small class="pull-<?=$left?> text-muted" dir="ltr"><?=$contained?></small>
               <?=$kudos?>
             </td>
           </tr>
@@ -32,31 +32,34 @@
         <div class="modal-content">
           <div class="modal-header">
             <button type="button" class="close pull-<?=$right?>" data-dismiss="modal" aria-hidden="true">&times;</button>
-            <h4 class="modal-title pull-<?=$left?>" id="file-name">&nbsp;</h4>
+            <h4 class="modal-title text-<?=$left?>" id="file-name">&nbsp;</h4>
           </div>
           <div class="modal-body"></div>
           <div class="modal-footer">
+            <div class="pull-<?=$left?>">
 <? if (($options['cdn']['highlight_js']) && ($options['cdn']['highlight_css'])) { ?>
-            <button type="button" class="btn btn-link highlight hidden pull-<?=$left?>"><?=_('Apply code highlighting')?></button>
-<? } ?>
-            <button type="button" class="btn btn-default" data-dismiss="modal"><?=_('Close')?></button>
+              <button type="button" class="btn btn-link highlight hidden"><?=_('Apply code highlighting')?></button>
+            </div>
+<? } ?>     <div class="pull-<?=$right?>">
+              <button type="button" class="btn btn-default" data-dismiss="modal"><?=_('Close')?></button>
 <? if ($options['general']['share_button']) { ?>
-            <div class="btn-group">
-              <a class="btn btn-primary fullview"></a>
-              <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">
-                <span class="caret"></span>
-                <span class="sr-only">Toggle Dropdown</span>
-              </button>
-              <ul class="dropdown-menu" role="menu">
+              <div class="btn-group">
+                <a class="btn btn-primary fullview"></a>
+                <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">
+                  <span class="caret"></span>
+                  <span class="sr-only">Toggle Dropdown</span>
+                </button>
+                <ul class="dropdown-menu" role="menu">
 <? if ($options['keys']['dropbox_app']) { ?>
-                <li><a class="save-dropbox">Save to Dropbox</a></li>
-                <li class="divider"></li>
+                  <li><a class="save-dropbox">Save to Dropbox</a></li>
+                  <li class="divider"></li>
 <? } ?>
-                <li><a class="email-link">Email</a></li>
-                <li><a class="facebook-link">Facebook</a></li>
-                <li><a class="google-link">Google+</a></li>
-                <li><a class="twitter-link">Twitter</a></li>
-              </ul>
+                  <li><a class="email-link">Email</a></li>
+                  <li><a class="facebook-link">Facebook</a></li>
+                  <li><a class="google-link">Google+</a></li>
+                  <li><a class="twitter-link">Twitter</a></li>
+                </ul>
+              </div>
             </div>
 <? } else { ?>
             <a class="btn btn-primary fullview"></a>
