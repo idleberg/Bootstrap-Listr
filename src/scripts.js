6e091329913795540132c9d26f6370f0ebafc0de
idleberg <jan@idleberg.com>
2014-09-02 14:42:07 +0200

    added check if library was loaded

--- src/scripts.js
+++ src/scripts.js
@@ -74,5 +74,15 @@ $(function() {
         var b = $(this).get(0).href;
         Dropbox.save(b);
     });
-    $("#bs-table").stupidtable();
+
+    if(jQuery().stupidtable) {
+      $("#bs-table").stupidtable();
+    }
+
+    if(jQuery().searcher) {
+      $("#bs-table").searcher({
+        inputSelector: "#search"
+      });
+    }
+    
 });
