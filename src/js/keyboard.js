52bd30a3cb988e1fa8958df8207b61c4d32855c8
Jan T. Sott <jan@idleberg.com>
2015-06-16 22:16:59 +0200

    seperate key events

--- src/js/keyboard.js
+++ src/js/keyboard.js
@@ -2,10 +2,21 @@ var hidden = $("tr.hidden");
 
 $(document).bind('keydown', function(event) {
 
-
+    // show hidden files
     if( event.altKey ) {
         $(hidden).addClass( "reveal" ).removeClass( "hidden");
     }
+    
 }).bind('keyup',function(){
-      $(hidden).removeClass( "reveal" ).addClass( "hidden");
+    
+    // hide hidden files again
+    $(hidden).removeClass( "reveal" ).addClass( "hidden");
+
+});
+
+$(document).bind('keyup', function(event) {
+    // focus search input
+    if (event.which === 70) {
+        $("#search").focus();
+    }
 });
