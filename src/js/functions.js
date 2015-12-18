1e0c416b4acb2d9816fd4fbbe78fa28b2b168149
idleberg <jan@idleberg.com>
2015-12-18 10:45:02 +0100

    add fix for hover-states

--- src/js/functions.js
+++ src/js/functions.js
@@ -1,17 +1,38 @@
 // update file counter
 var column = $('#file-count').index();
 var striped_bg = $('.table-striped > tbody > tr:nth-of-type(odd)').css('background-color');
+var hover_bg;
 
+// CSS fix for striped tables
+var stripedRows = function() {
+    if ($('#listr-table').has('.table-striped')) {
+        $('tbody tr').css( "background-color", "inherit");
+        $('tbody tr:not(.hidden):even').css( "background-color", striped_bg );
+    }
+}
+
+// Adjust counter for rows
 var countRows = function() {
     $('tbody tr:not(.hidden) > td:nth-of-type('+column+1+')').each(function(index,elem){
         index+=1;
         $(this).text(index);
     });
 
-    // fix for striped tables
-    if ($('#listr-table').has('.table-striped')) {
-        $('tbody tr').css("background-color", "inherit");
-        $('tbody tr:not(.hidden):odd').css( "background-color", striped_bg );
-    }
+    stripedRows();
 };
 countRows();
+
+// CSS fix for .table-hover
+$('.table-hover>tbody>tr').bind({
+  mouseenter: function(e) {
+    if( !hover_bg ) {
+        $(this).removeAttr('style');
+        hover_bg = $(this).css('background-color');
+    }
+    console.log(hover_bg);
+    $(this).css( "background-color", hover_bg);
+  },
+  mouseleave: function(e) {
+    stripedRows();
+  }
+})
