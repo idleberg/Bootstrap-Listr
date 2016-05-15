8b30eba3fba4d0c6f2864e5b00a4e3f2edf898a2
Jan T. Sott <jan@idleberg.com>
2016-05-15 13:50:21 +0200

    rename var; indent

--- src/js/keyboard.js
+++ src/js/keyboard.js
@@ -1,4 +1,4 @@
-var k,
+var K,
 Keyboard = {
 
   config: {
@@ -7,117 +7,116 @@ Keyboard = {
       search: $("#listr-search")
   },
 
-    init: function() {
-        m = this.config;
-        this.events();
-    },
+  init: function() {
+      K = this.config;
+      this.events();
+  },
 
-    // Keyboard events
-    events: function() {
+  // Keyboard events
+  events: function() {
+    $(document).bind('keydown', function(event) {
+      Keyboard.revealFiles();
+    }).bind('keyup',function(){
+        Keyboard.hideFiles();
+    });
 
-      $(document).bind('keydown', function(event) {
-        Keyboard.revealFiles();
-      }).bind('keyup',function(){
-          Keyboard.hideFiles();
-      });
+    $(document).bind('keyup', function(event) {
+        Keyboard.playerControls();
+    })
 
-      $(document).bind('keyup', function(event) {
-          Keyboard.playerControls();
-      })
+    $(document).bind('keyup', function(event) {
+        Keyboard.focusSearch();
+    })
+  },
 
-      $(document).bind('keyup', function(event) {
-          Keyboard.focusSearch();
-      })
-    },
+  // show hidden files
+  revealFiles: function() {
+      if( event.altKey ) {
+          $(K.hidden).addClass( "reveal" ).removeClass( "hidden-xs-up");
+          stripedRows();
+      }
+  },
 
-    // show hidden files
-    revealFiles: function() {
-        if( event.altKey ) {
-            $(m.hidden).addClass( "reveal" ).removeClass( "hidden-xs-up");
-            stripedRows();
+  // hide hidden files again
+  hideFiles: function() {
+      $(K.hidden).removeClass( "reveal" ).addClass( "hidden-xs-up");
+      stripedRows();
+  },
+
+  // focus search input
+  focusSearch: function() {
+      if (!viewer.hasClass('in')) {
+        if (event.which === 70) {
+            $(K.search).focus();
+            $(document).scrollTop(0);
         }
-    },
+      }
+  },
 
-    // hide hidden files again
-    hideFiles: function() {
-        $(m.hidden).removeClass( "reveal" ).addClass( "hidden-xs-up");
-        stripedRows();
-    },
+  // Control HTML5 player
+  playerControls: function() {
+    // Only when modal is visible
+    if (viewer.hasClass('in')) {
 
-    // focus search input
-    focusSearch: function() {
-        if (!viewer.hasClass('in')) {
-         if (event.which === 70) {
-             $(m.search).focus();
-             $(document).scrollTop(0);
-         }
+      // Fullscreen
+      if (event.which === 70) {
+        if (player.requestFullscreen) {
+          player.requestFullscreen();
+        } else if (player.msRequestFullscreen) {
+          player.msRequestFullscreen();
+        } else if (player.mozRequestFullScreen) {
+          player.mozRequestFullScreen();
+        } else if (player.webkitRequestFullscreen) {
+          player.webkitRequestFullscreen();
         }
-    },
+      }
 
-    // Control HTML5 player
-    playerControls: function() {
-        // Only when modal is visible
-        if (viewer.hasClass('in')) {
+      // Play/pause
+      if (event.which === 32) {
 
-          // Fullscreen
-          if (event.which === 70) {
-            if (player.requestFullscreen) {
-              player.requestFullscreen();
-            } else if (player.msRequestFullscreen) {
-              player.msRequestFullscreen();
-            } else if (player.mozRequestFullScreen) {
-              player.mozRequestFullScreen();
-            } else if (player.webkitRequestFullscreen) {
-              player.webkitRequestFullscreen();
-            }
-          }
+        event.preventDefault();
 
-          // Play/pause
-          if (event.which === 32) {
-
-            event.preventDefault();
-
-            if (player.paused === false) {
-              player.pause();
-            } else {
-              player.play();
-            }
-          }
-
-          // Seek backward
-          if (event.which === 37) {
-            player.currentTime -= 1;
-          }
-
-          // Seek forward
-          if (event.which === 39) {
-            player.currentTime += 1;
-          }
-
-          // Rewind player
-          if (event.which === 37 && event.shiftKey) {
-            player.currentTime = 0;
-          }
-
-          // Increase volume
-          if (event.which === 38) {
-            player.volume += 0.1;
-          }
-
-          // Max volume
-          if (event.which === 38 && event.shiftKey) {
-            player.volume = 1;
-          }
-
-          // Decrease volume
-          if (event.which === 40) {
-            player.volume -= 0.1;
-          }
-
-          // Mute volume
-          if (event.which === 40 && event.shiftKey) {
-            player.volume = 0;
-          }
+        if (player.paused === false) {
+          player.pause();
+        } else {
+          player.play();
         }
+      }
+
+      // Seek backward
+      if (event.which === 37) {
+        player.currentTime -= 1;
+      }
+
+      // Seek forward
+      if (event.which === 39) {
+        player.currentTime += 1;
+      }
+
+      // Rewind player
+      if (event.which === 37 && event.shiftKey) {
+        player.currentTime = 0;
+      }
+
+      // Increase volume
+      if (event.which === 38) {
+        player.volume += 0.1;
+      }
+
+      // Max volume
+      if (event.which === 38 && event.shiftKey) {
+        player.volume = 1;
+      }
+
+      // Decrease volume
+      if (event.which === 40) {
+        player.volume -= 0.1;
+      }
+
+      // Mute volume
+      if (event.which === 40 && event.shiftKey) {
+        player.volume = 0;
+      }
     }
+  }
 };
